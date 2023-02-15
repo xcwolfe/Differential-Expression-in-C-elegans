ae7fa4936206dd6947bd1a56d93e5719e14b97bb
Zach Wolfe <64483133+xcwolfe@users.noreply.github.com>
2023-02-15 14:02:09 -0500

    Create JUM workflow.c

---
+++ JUM workflow.c
@@ -0,0 +1,10 @@
+# This pipeline illustrates how to run JUM using a large cluster computer (with hopefully lots of memory). We will be utilizing Unix (C language) and later R to analyze our JUM data
+
+# Navigate to your working directory:
+
+`cd /work/users/zwolfe`
+
+# Make a folder to put all your fastq files in:
+
+` mkdir srrfiles`
+`cd srrfiles/`

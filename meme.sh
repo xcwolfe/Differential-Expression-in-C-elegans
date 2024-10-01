389c764a11e766cf94c50f7ab30136add6af83c1
Zach Wolfe <64483133+xcwolfe@users.noreply.github.com>
2024-10-01 13:09:46 -0500

    Update meme.sh

--- meme.sh
+++ meme.sh
@@ -20,3 +20,16 @@ $ meme shuffled_AVM_cassette_MEME.tsv.fa -dna -oc . -nostatus -time 14400 -mod a
 ```
 $ mast meme.xml shuffled_AVM_cassette_MEME.tsv.fa -oc . -nostatus
 ```
+
+Copyright 2024 The Regents of the University of California
+
+All Rights Reserved
+
+Created by Zachery Wolfe
+
+Department of Biochemistry
+
+This file is part of Differential Expression in C. elegans. \
+Differential Expression in C. elegans is a free pipeline: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. \
+Differential Expression in C. elegans is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. \
+You should have received a copy of the GNU General Public License along with Differential Expression in C. elegans. If not, see <https://www.gnu.org/licenses/>.
